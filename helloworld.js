9a4cc8c7e7f1d0d430463d377eefc37859617cd0
Jari Kovalainen <itse@jarikovalainen.com>
2022-03-18 18:47:28 +0200

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,14 @@
 /* Create a simple web server outputting “hello world” */
+
+/* Your app should respond to different URL’s by serving different content to the browser. This can be done by studying the request.url –parameter.*/
+
+
+if (request.url === “/”){
+
+// do something
+} 
+else if (request.url === “/helloworld”){
+// do something
+} 
+
+console.log('Server running on port: 8000');
